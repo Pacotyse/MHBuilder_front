d0278b5156d454d3d9601a0e545612380cb407e3
Joshua <webnjosh@gmail.com>
2023-05-09 12:13:42 +0400

    build (routes) : setting React Router and main routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import reactLogo from '../../assets/react.svg';
 import './styles.scss';
 
 function App() {
   return (
     <div className="app">
-      <h1 className="app__title">Vite + React</h1>
-      <img src={reactLogo} alt="react logo" />
+      <h1 className="app__title">Monster Hunter Builder</h1>
+      {/* React router prevent the app from refreshing when navigating */}
+      <Routes>
+        <Route path="/" element={<h2>Accueil</h2>} />
+        <Route path="/builder" element={<h2>Builder</h2>} />
+        <Route path="/loadouts" element={<h2>Loadouts</h2>} />
+        <Route path="/login" element={<h2>Login</h2>} />
+        <Route path="/sign-in" element={<h2>Sign-in</h2>} />
+        <Route path="/users/:id" element={<h2>Profile</h2>} />
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
+
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-// On importe ReactDom qui nous permettra d'injecter notre application dans le DOM
+// ReactDOM insert the app into the DOM
 import ReactDOM from 'react-dom/client';
-// On importe notre composant principal
+// Allows routing for the app
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-// On importe notre fichier de style global
 import './styles/index.scss';
 
-// Je créer un root pour mon application (a partir d'un élément HTML)
+// Create a root element for the app
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 // On injecte notre application dans le DOM
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
