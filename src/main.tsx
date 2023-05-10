3b2479866a0c49d52e0203a482fd8831f8217adf
Joshua <webnjosh@gmail.com>
2023-05-10 16:25:31 +0400

    build : redux settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// Provider enables the state to be available all over the app through the store
+import { Provider } from 'react-redux';
 // ReactDOM insert the app into the DOM
 import ReactDOM from 'react-dom/client';
 // Allows routing for the app
@@ -5,12 +7,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './styles/index.scss';
 
+import store from './store';
+
 // Create a root element for the app
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-// On injecte notre application dans le DOM
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
+
+// Je déduis le type `RootState` et `AppDispatch` depuis le store lui même
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
