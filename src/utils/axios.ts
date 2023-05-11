707530c91ff64392aa9dfe3803adbfc8eed71942
Joshua <webnjosh@gmail.com>
2023-05-11 15:29:45 +0400

    build: add axios and config baseURL

---
+++ src/utils/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// Create an axios instance so the base URL is define here
+// When fetching, we only need to specify the route
+
+// eslint-disable-next-line import/prefer-default-export
+export const axiosInstance = axios.create({
+  baseURL: 'https://test-production-f834.up.railway.app/',
+});
