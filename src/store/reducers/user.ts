cf22d9d34831f094f04817b68af57a9a7bcecef2
Kevin moutou <kevmoutou@gmail.com>
2023-05-15 13:51:28 +0200

    feat (userRedux): add post userLogin

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -1,9 +1,14 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { createAppAsyncThunk } from '../../utils/redux';
 import { axiosInstance } from '../../utils/axios';
+import { UserResponse } from '../../@types/user';
 
 interface UserState {
+  username: string;
+  token: string;
+  isLogged: boolean;
+  isLoading: boolean;
+  errorMessage: string;
   credentials: {
     email: string;
     password: string;
@@ -11,6 +16,11 @@ interface UserState {
 }
 
 export const initialState: UserState = {
+  username: '',
+  token: '',
+  isLogged: false,
+  isLoading: false,
+  errorMessage: '',
   credentials: {
     email: '',
     password: '',
@@ -19,16 +29,45 @@ export const initialState: UserState = {
 
 export const changeCredentialsField = createAction<{
   value: string;
-  // keyof permet de récupérer les clés d'un objet
-  // (les valeurs littérales ici 'email' et 'password')
+  // keyof credentials can be 'email' or 'password
   field: keyof UserState['credentials'];
 }>('user/CHANGE_CREDENTIALS_FIELD');
+
+export const login = createAppAsyncThunk(
+  'user/LOGIN',
+  async (_, thunkAPI) => {
+    // get the state here
+    const state = thunkAPI.getState();
+    // Extract email and password from credentials
+    const { email, password } = state.user.credentials;
+    // Send data to back
+    const { data } = await axiosInstance.post('/login', {
+      email,
+      password,
+    });
+    return data as UserResponse;
+  },
+);
 
 const userReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeCredentialsField, (state, action) => {
       const { field, value } = action.payload;
       state.credentials[field] = value;
+    })
+    .addCase(login.pending, (state) => {
+      state.isLoading = true;
+      state.errorMessage = '';
+    })
+    .addCase(login.fulfilled, (state, action) => {
+      state.username = action.payload.username;
+      state.token = action.payload.token;
+      state.isLoading = false;
+      state.isLogged = true;
+    })
+    .addCase(login.rejected, (state) => {
+      state.isLoading = false;
+      state.errorMessage = 'Login or password is incorrect';
     });
 });
 
