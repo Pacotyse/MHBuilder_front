2fb7089284d65a3d619b6537ceafd8dc9b039035
Kevin moutou <kevmoutou@gmail.com>
2023-05-22 00:54:40 +0200

    feat(HomePage) added basic responsive

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 import BuilderPage from '../BuilderPage';
 import './styles.scss';
-// import Footer from '../../components/Footer/Footer';
 import FirstSection from '../../components/Main/FirstSection';
 import SecondSection from '../../components/Main/SecondSection';
 import DiscordSection from '../../components/Main/DiscordSection';
-import LastestUpdate from '../../components/Main/LastestUpdate';
+import LatestUpdate from '../../components/Main/LatestUpdate';
 import Footer from '../../components/Footer/Footer';
 
 function HomePage() {
@@ -15,7 +14,7 @@ function HomePage() {
       <FirstSection />
       <SecondSection />
       <DiscordSection />
-      <LastestUpdate />
+      <LatestUpdate />
       <Footer />
 
     </div>
---
+++ src/components/Main/LatestUpdate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LatestUpdate.scss';
+// import { Button } from '../Button';
+
+function LatestUpdate() {
+  return (
+    <div className="latestUpdate-container">
+      <h1 className="latestUpdate-h1"> Latest Update</h1>
+      <p className="latestUpdate-p">Lorem Ipsum fjsolkdfjskldf slkdfjsl dfkjsldfk</p>
+    </div>
+  );
+}
+
+export default LatestUpdate;
