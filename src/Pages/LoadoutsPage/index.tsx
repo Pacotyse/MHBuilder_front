595d1f8a92b860cbff00a1c2efa9dbcbabee3de8
Joshua <webnjosh@gmail.com>
2023-05-25 15:47:18 +0400

    fix (loadout page): bug on submit form

--- src/Pages/LoadoutsPage/index.tsx
+++ src/Pages/LoadoutsPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles.scss';
 
@@ -21,7 +21,8 @@ function Loadouts() {
     dispatch(fetchAllLoadouts());
   }, [dispatch]);
 
-  function handleGetOneLoadout() {
+  function handleGetOneLoadout(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     dispatch(importLoadoutById());
     // go to builder page
     navigate('/builder');
@@ -35,7 +36,10 @@ function Loadouts() {
         <h2 className="loadouts-search_title">FIND BY CODE</h2>
         <p className="loadouts-search__description">Got loadout codes? Paste them below to get them to the builder and see details!</p>
 
-        <form className="loadouts-search__form">
+        <form
+          className="loadouts-search__form"
+          onSubmit={handleGetOneLoadout}
+        >
           <input
             type="text"
             placeholder="Past your code here"
@@ -44,7 +48,7 @@ function Loadouts() {
               dispatch(setLoadoutCodeField(event.target.value));
             }}
           />
-          <button type="button" className="loadouts-search__submit-button" onClick={handleGetOneLoadout}>Go</button>
+          <button type="submit" className="loadouts-search__submit-button">Go</button>
         </form>
       </div>
       {/* //? SECTION all loadouts and filter */}
